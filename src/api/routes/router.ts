6c5af6c12f0422b4ac9ad9f9d403cd9531f870fd
hugo miranda <hugo.miranda.souza@gmail.com>
2022-03-17 18:15:38 -0300

    change message from route /

--- src/api/routes/router.ts
+++ src/api/routes/router.ts
@@ -4,5 +4,5 @@ import { cepRouter } from "./cep.router.ts";
 
 export const applicationRouter = new oak.Router();
 
-applicationRouter.get("/", (context) => context.response.body = "Hello World!");
+applicationRouter.get("/", (context) => context.response.body = "API Alive!");
 applicationRouter.use("/api", cepRouter.routes());
